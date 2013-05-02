c7405f47fcf64c559a1303f60ca0dfaa42f486eb
Trey Hunner <trey@treyhunner.com>
2013-05-01 21:23:32 -0700

    Remove unused imports in urls file

--- email_log/tests/urls.py
+++ email_log/tests/urls.py
@@ -1,7 +1,4 @@
-try:
-    from django.conf.urls import patterns, include, url
-except ImportError:
-    from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 admin.autodiscover()
