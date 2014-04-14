93dedcca44fec103466bfb0e7a94d90dcad83ad9
Trey Hunner <trey@treyhunner.com>
2014-04-14 13:48:28 -0400

    Add to __path__ based on desired migrations module

--- email_log/__init__.py
+++ email_log/__init__.py
@@ -1,1 +1,16 @@
+import os
+
 default_app_config = 'email_log.apps.EmailLogConfig'
+
+current_dir = os.path.dirname(__file__)
+
+
+# Ensure the user is not using Django 1.6 or below with South
+try:
+    from django.db import migrations  # noqa
+except ImportError:
+    migrations_dir = os.path.join(current_dir, 'south')
+else:
+    migrations_dir = os.path.join(current_dir, 'nonsouth')
+
+__path__.append(migrations_dir)
