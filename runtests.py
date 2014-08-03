ef89d3608b9ab54aef105528f2c15fa9cc437bcd
Trey Hunner <trey@treyhunner.com>
2014-08-02 23:08:22 -0700

    Fix tests for Django 1.7

--- runtests.py
+++ runtests.py
@@ -3,6 +3,7 @@ import sys
 from os.path import abspath, dirname
 
 from django.conf import settings
+import django
 
 
 sys.path.insert(0, abspath(dirname(__file__)))
@@ -29,8 +30,18 @@ if not settings.configured:
 
 
 def runtests():
-    from django.test.simple import DjangoTestSuiteRunner
-    failures = DjangoTestSuiteRunner(failfast=False).run_tests(['tests'])
+    if hasattr(django, 'setup'):
+        django.setup()
+    try:
+        from django.test.runner import DiscoverRunner
+        runner_class = DiscoverRunner
+        test_args = ['email_log.tests']
+    except ImportError:
+        from django.test.simple import DjangoTestSuiteRunner
+        runner_class = DjangoTestSuiteRunner
+        test_args = ['tests']
+
+    failures = runner_class(failfast=False).run_tests(test_args)
     sys.exit(failures)
 
 
